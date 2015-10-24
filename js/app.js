226f5bd49c3e4f78b9e434886eda9164764a444b
olagajda <aleksandra.gajda@gmail.com>
2015-10-24 19:44:24 +0200

    added purple hexagons on hover in top and advantages sections

--- js/app.js
+++ js/app.js
@@ -1,54 +1,70 @@
-$(function() {
+var Application = function() {
 
-    var Application = function() {
+    function init() {
+        console.log("init");
+    }
 
-        function init() {
-            console.log("init");
-        }
 
-        function scrollPage() {
-            console.log("scrolling");
-        }
+    function changeImageOnHover() {
 
-        return {
-            init:init
-        };
+        var shapeGray1 = $(".hexagon-gray-1");
+        var shapeGray2 = $(".hexagon-gray-2");
+        var shapeGray3 = $(".hexagon-gray-3");
+        var shapeGray4 = $(".hexagon-gray-4");
 
+        shapeGray1.on("mouseenter", function(){
+            $(this).find("img").attr("src", "images/icons/heart-purple.svg");
+        });
+        shapeGray1.on("mouseleave", function(){
+            $(this).find("img").attr("src", "images/icons/heart.svg");
+        });
+
+        shapeGray2.on("mouseenter", function(){
+            $(this).find("img").attr("src", "images/icons/diamond-purple.svg");
+        });
+        shapeGray2.on("mouseleave", function(){
+            $(this).find("img").attr("src", "images/icons/diamond.svg");
+        });
+
+        shapeGray3.on("mouseenter", function(){
+            $(this).find("img").attr("src", "images/icons/cup-purple.svg");
+        });
+        shapeGray3.on("mouseleave", function(){
+            $(this).find("img").attr("src", "images/icons/cup.svg");
+        });
+
+        shapeGray4.on("mouseenter", function(){
+            $(this).find("img").attr("src", "images/icons/phone-purple.svg");
+        });
+        shapeGray4.on("mouseleave", function(){
+            $(this).find("img").attr("src", "images/icons/phone.svg");
+        });
+
+    }
+
+
+    function scrollPage() {
+        console.log("scrolling");
+    }
+
+    return {
+        init:init,
+        changeImageOnHover:changeImageOnHover
     };
 
+};
+
+
+
+
+
+$(function() {
 
 
     var app = new Application();
     app.init();
+    app.changeImageOnHover();
 
 
-    //jQuery('img.svg').each(function(){
-    //    var $img = jQuery(this);
-    //    var imgID = $img.attr('id');
-    //    var imgClass = $img.attr('class');
-    //    var imgURL = $img.attr('src');
-    //
-    //    jQuery.get(imgURL, function(data) {
-    //        // Get the SVG tag, ignore the rest
-    //        var $svg = jQuery(data).find('svg');
-    //
-    //        // Add replaced image's ID to the new SVG
-    //        if(typeof imgID !== 'undefined') {
-    //            $svg = $svg.attr('id', imgID);
-    //        }
-    //        // Add replaced image's classes to the new SVG
-    //        if(typeof imgClass !== 'undefined') {
-    //            $svg = $svg.attr('class', imgClass+' replaced-svg');
-    //        }
-    //
-    //        // Remove any invalid XML tags as per http://validator.w3.org
-    //        $svg = $svg.removeAttr('xmlns:a');
-    //
-    //        // Replace image with new SVG
-    //        $img.replaceWith($svg);
-    //
-    //    }, 'xml');
-    //
-    //});
 });
 
