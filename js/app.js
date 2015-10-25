cf990d7034100518f7e1b82989fb2e534e476a8e
olagajda <aleksandra.gajda@gmail.com>
2015-10-25 12:18:36 +0100

    added sticky menu

--- js/app.js
+++ js/app.js
@@ -43,13 +43,99 @@ var Application = function() {
     }
 
 
+    function stickyMenu() {
+
+        var menu = $(".sticky-menu");
+        var paragraphs = $(".chapter");
+        var links = $(".sticky-menu li");
+        var lastPositionTop = 0;
+        var menuHeight = menu.height();
+
+        console.log(paragraphs);
+        console.log(links);
+
+        $(window).scroll(function (event) {
+
+            if (menu.hasClass("sticky") === false && (menu.offset().top) < ($(this).scrollTop())) {
+
+
+                lastPositionTop = menu.offset().top;
+                menu.addClass("sticky");
+            }
+
+            if (menu.hasClass("sticky") && (lastPositionTop > ($(this).scrollTop()))) {
+
+                menu.removeClass("sticky");
+            }
+
+            paragraphs.each(function (index) {
+
+                if (index + 1 >= paragraphs.length) {
+
+                    if (paragraphs.eq(index).offset().top < $(window).scrollTop()) {
+
+                        links.eq(index).addClass("active");
+                    }
+
+                    else {
+                        links.eq(index).removeClass("active");
+                    }
+
+
+                }
+                else {
+
+                    if (paragraphs.eq(index).offset().top < $(window).scrollTop() &&
+                        paragraphs.eq(index + 1).offset().top > $(window).scrollTop()) {
+
+                        links.eq(index).addClass("active");
+                    }
+
+                    else {
+                        links.eq(index).removeClass("active");
+                    }
+                }
+            });
+        });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     function scrollPage() {
         console.log("scrolling");
     }
 
     return {
         init:init,
-        changeImageOnHover:changeImageOnHover
+        changeImageOnHover:changeImageOnHover,
+        stickyMenu:stickyMenu
     };
 
 };
@@ -64,6 +150,8 @@ $(function() {
     var app = new Application();
     app.init();
     app.changeImageOnHover();
+    app.stickyMenu();
+
 
 
 });
